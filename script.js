d982b2a0d3c0505c47ed8264af2ebefe9076e346
Gushihiro <robinsonhiroto@gmail.com>
2021-04-02 02:06:03 -0700

    figured out how to automate with arrays for better looking code

--- script.js
+++ script.js
@@ -17,38 +17,23 @@
     //then iterage over all hour elements
     //using an id/data attribute, determine what hour the current timeblock represents
     //if the current element time is less than moment time, it's grey
-var saveBtn1 = $("#save1")
-var saveBtn2 = $("#save2")
-var saveBtn3 = $("#save3")
-var saveBtn4 = $("#save4")
-var saveBtn5 = $("#save5")
-var saveBtn6 = $("#save6")
-var saveBtn7 = $("#save7")
-var saveBtn8 = $("#save8")
-var saveBtn9 = $("#save9")
-
+var saveBtns = [$("#save1"), $("#save2"), $("#save3"), $("#save4"), $("#save5"), $("#save6"), $("#save7"), $("#save8"), $("#save9")]
+var selectedHour = [
+    $("textarea#9"), $("textarea#10"), $("textarea#11"), 
+    $("textarea#12"), $("textarea#13"), $("textarea#14"), 
+    $("textarea#15"), $("textarea#16"), $("textarea#17")
+]
+var storedHour = ['hour9', 'hour10', 'hour11', 'hour12', 'hour13', 'hour14', 'hour15', 'hour16', 'hour17']
+console.log(saveBtns)
 var currentTime = parseInt(moment().format("H"));
 var currentDate = $("#currentDay")
     currentDate.append(moment().format("dddd, MMMM Do"))
 
-var savedTask1 = localStorage.getItem("hour9")
-    $("textarea#9").append(savedTask1)
-var savedTask2 = localStorage.getItem("hour10")
-    $("textarea#10").append(savedTask2)
-var savedTask3 = localStorage.getItem("hour11")
-    $("textarea#11").append(savedTask3)
-var savedTask4 = localStorage.getItem("hour12")
-    $("textarea#12").append(savedTask4)
-var savedTask5 = localStorage.getItem("hour13")
-    $("textarea#13").append(savedTask5)
-var savedTask6 = localStorage.getItem("hour14")
-    $("textarea#14").append(savedTask6)
-var savedTask7 = localStorage.getItem("hour15")
-    $("textarea#15").append(savedTask7)
-var savedTask8 = localStorage.getItem("hour16")
-    $("textarea#16").append(savedTask8)
-var savedTask9 = localStorage.getItem("hour17")
-    $("textarea#17").append(savedTask9)
+for (let i = 0; i < selectedHour.length; i++) {
+    var finalStore = localStorage.getItem(storedHour[i]);
+        selectedHour[i].append(finalStore);   
+}
+console.log(storedHour)
 function assignTimePast() {
     if (parseInt($("#9").attr("id")) < currentTime) {
         $("textarea#9").attr("class", "past col-10")
@@ -161,61 +146,71 @@ function assignTimeFuture() {
     }
 }
 
-function saveTask1() {
-    localStorage.setItem("hour9", $("textarea#9").val())
-}
-function saveTask2() {
-    localStorage.setItem("hour10", $("textarea#10").val())
-}
-function saveTask3() {
-    localStorage.setItem("hour11", $("textarea#11").val())
-}
-function saveTask4() {
-    localStorage.setItem("hour12", $("textarea#12").val())
-}
-function saveTask5() {
-    localStorage.setItem("hour13", $("textarea#13").val())
-}
-function saveTask6() {
-    localStorage.setItem("hour14", $("textarea#14").val())
-}
-function saveTask7() {
-    localStorage.setItem("hour15", $("textarea#15").val())
-}
-function saveTask8() {
-    localStorage.setItem("hour16", $("textarea#16").val())
-}
-function saveTask9() {
-    localStorage.setItem("hour17", $("textarea#17").val())
-}
+var savedTasks = [
+    function saveTask1() {
+        localStorage.setItem("hour9", $("textarea#9").val())
+    },
+    function saveTask2() {
+        localStorage.setItem("hour10", $("textarea#10").val())
+    },
+    function saveTask3() {
+        localStorage.setItem("hour11", $("textarea#11").val())
+    },
+    function saveTask4() {
+        localStorage.setItem("hour12", $("textarea#12").val())
+    },
+    function saveTask5() {
+        localStorage.setItem("hour13", $("textarea#13").val())
+    },
+    function saveTask6() {
+        localStorage.setItem("hour14", $("textarea#14").val())
+    },
+    function saveTask7() {
+        localStorage.setItem("hour15", $("textarea#15").val())
+    },
+    function saveTask8() {
+        localStorage.setItem("hour16", $("textarea#16").val())
+    },
+    function saveTask9() {
+        localStorage.setItem("hour17", $("textarea#17").val())
+    }
+]
+//saveBtn1.on("click", function() {
+//   saveTask1();
+//})
+//saveBtn2.on("click", function() {
+//    saveTask2();
+//})
+//saveBtn3.on("click", function() {
+ //   saveTask3();
+//})
+//saveBtn4.on("click", function() {
+//    saveTask4();
+//})
+//saveBtn5.on("click", function() {
+//    saveTask5();
+//})
+//saveBtn6.on("click", function() {
+ //   saveTask6();
+//})
+//saveBtn7.on("click", function() {
+ //   saveTask7();
+//})
+//saveBtn8.on("click", function() {
+//    saveTask8();
+//})
+//saveBtn9.on("click", function() {
+ //   saveTask9();
+//})
 
-saveBtn1.on("click", function() {
-   saveTask1();
-})
-saveBtn2.on("click", function() {
-    saveTask2();
-})
-saveBtn3.on("click", function() {
-    saveTask3();
-})
-saveBtn4.on("click", function() {
-    saveTask4();
-})
-saveBtn5.on("click", function() {
-    saveTask5();
-})
-saveBtn6.on("click", function() {
-    saveTask6();
-})
-saveBtn7.on("click", function() {
-    saveTask7();
-})
-saveBtn8.on("click", function() {
-    saveTask8();
-})
-saveBtn9.on("click", function() {
-    saveTask9();
-})
+for (let i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].on("click", function() {
+        savedTasks[i]()
+        console.log(saveBtns[i])
+        console.log(savedTasks[i])
+    })
+    
+}
 
 assignTimePast();
 assignTimePresent();
